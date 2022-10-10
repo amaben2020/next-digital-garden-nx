8673ac5541f536397d7515e5dd5988e157627bfc
algomachine007 <amaben202phantom@gmail.com>
2022-10-10 14:53:18 +0100

    configuring a performant tailwind app

--- tailwind-workspace-preset.js
+++ tailwind-workspace-preset.js
@@ -1,10 +1,10 @@
+// having a global configuration for all teams in the monorepo
 const { join } = require('path');
 
 const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
   purge: [
     join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
     ...createGlobPatternsForDependencies(__dirname),
@@ -14,4 +14,5 @@ module.exports = {
     extend: {},
   },
   plugins: [require('@tailwindcss/typography')],
+  presets: [require('./../domsmith/apps/site/tailwind.config')],
 };
